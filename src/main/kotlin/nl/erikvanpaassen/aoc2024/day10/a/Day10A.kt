fe7add04faa548e49cf4301bb83416f07528cef5
Erik van Paassen <erik@evpwebdesign.nl>
2024-12-10 16:32:21 +0100

    Day 10

---
+++ src/main/kotlin/nl/erikvanpaassen/aoc2024/day10/a/Day10A.kt
@@ -0,0 +1,52 @@
+package nl.erikvanpaassen.aoc2024.day10.a
+
+import java.io.File
+
+fun main() {
+    val input = File("input/day10a.txt").readLines()
+
+    val topographicMap = TopographicMap.new(input)
+
+    val totalScore = topographicMap.getTrailheads().sumOf { th -> topographicMap.findReachableNines(th).count() }
+    println(totalScore)
+}
+
+data class Coordinates(val y: Int, val x: Int) {
+    fun up() = Coordinates(y - 1, x)
+    fun down() = Coordinates(y + 1, x)
+    fun left() = Coordinates(y, x - 1)
+    fun right() = Coordinates(y, x + 1)
+}
+
+class TopographicMap(private val grid: Array<Array<Int>>) {
+
+    fun get(coordinates: Coordinates) = grid[coordinates.y][coordinates.x]
+
+    fun getTrailheads(): List<Coordinates> {
+        return grid.flatMapIndexed { y, row ->
+            row.mapIndexed { x, height -> if (height == 0) Coordinates(y, x) else null }
+        }.filterNotNull()
+    }
+
+    fun findReachableNines(position: Coordinates): Set<Coordinates> {
+        val currentHeight = get(position)
+
+        return if (currentHeight == 9) {
+            setOf(position)
+        } else {
+            listOf(position.up(), position.down(), position.left(), position.right())
+                .filter(::contains)
+                .filter { c -> get(c) == currentHeight + 1 }
+                .flatMap(::findReachableNines)
+                .toSet()
+        }
+    }
+
+    fun contains(coordinates: Coordinates): Boolean {
+        return coordinates.y >= 0 && coordinates.y < grid.count() && coordinates.x >= 0 && coordinates.x < grid[0].count()
+    }
+
+    companion object {
+        fun new(lines: List<String>) = TopographicMap(lines.map { line -> line.toCharArray().map(Character::getNumericValue).toTypedArray() }.toTypedArray())
+    }
+}
